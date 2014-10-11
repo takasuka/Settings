feb00bc9dd6c4156d405d39065c1a37edcd92782
Yu Takasuka <github@takasuka.net>
2014-10-12 04:13:19 +0800

    add symlink

--- symlink/make_symlink.sh
+++ symlink/make_symlink.sh
@@ -2,3 +2,4 @@ ln -sf ~/Settings/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Settings/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Settings/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/Settings/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/Dropbox/bin ~/
