3a72188e54ce78ac3f32b6f7100ca617483c6ed2
Yu Takasuka <github@takasuka.net>
2015-03-20 13:22:55 +0800

    Delete zshrc link

--- symlink/make_symlink.sh
+++ symlink/make_symlink.sh
@@ -1,5 +1,4 @@
 ln -sf ~/Settings/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Settings/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Settings/dotfiles/.gitignore ~/.gitignore
-ln -sf ~/Settings/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/Dropbox/bin ~/
